0eab9a86c23c2c7e1059b10f4dad5c3ff6dec484
midamnuvas <ekoeko0654@gmail.com>
2021-12-12 22:28:35 +0300

    the  r a w  experience

--- kilo.c
+++ kilo.c
@@ -22,8 +22,11 @@ void enable_raw_mode() {
     /* disable
      * IXON     Ctrl-s and Ctrl-q
      * ICRNL    carriage return new line
+     * BRKINT
+     * INPCK
+     * ISTRIP
      */
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | INPCK | ISTRIP | ICRNL | IXON);
 
 
     /* disable
@@ -32,14 +35,23 @@ void enable_raw_mode() {
     raw.c_oflag &= ~(OPOST);
 
 
+    raw.c_lflag |= (CS8);
+
+
     /* disable
      * ECHO     echo
      * ICANON   canonical mode
      * ISIG     Ctrl-c and Ctrl-v signals
      * IEXTEN   Ctrl-v
-     *
      */
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+
+    /* minimum number of bytes required for read() to return. */
+    raw.c_cc[VMIN] = 0;
+
+    /* max amount of time to wait before read() returns. */
+    raw.c_cc[VTIME] = 1;
 
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -51,13 +63,17 @@ int main() {
     enable_raw_mode();
 
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while ( == 1 && c != 'q') {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
+        
+        if (c == 'q') break;
     }
 
 
