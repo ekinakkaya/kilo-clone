3822b4fa92823ca0556d6a0a866100e20663644a
midamnuvas <ekoeko0654@gmail.com>
2021-12-13 23:48:10 +0300

    Drawing tildes.

--- kilo.c
+++ kilo.c
@@ -102,10 +102,24 @@ void editor_process_keypress() {
 
 
 
+void editor_draw_rows() {
+    int y;
+    for (y = 0; y < 24; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+
+
 void editor_refresh_screen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editor_draw_rows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
 }
+
 
 
 int main() {
